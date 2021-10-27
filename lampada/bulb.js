ee15facabe6722baea8b10659747dc7f9cc4137b
Fabiano de Sousa Lima <fabianosl.dev@gmail.com>
2021-10-26 22:02:50 -0300

    repo

--- lampada/bulb.js
+++ lampada/bulb.js
@@ -2,7 +2,3 @@ function changeImage() {
     var image = document.getElementById('myImage');
     image.src.match('bulb-on') ? image.src = 'bulb-off.png' : image.src = 'bulb-on.png';
 }
-
-function sum(a, b) {
-    return a + b
-}
